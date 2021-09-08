ae7333adfdca6904d900dfd3f3c4c6074d13e97b
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 12:59:00 +0200

    'Update'

--- LocateAndInspect/scripts/LocateAndInspect.lua
+++ LocateAndInspect/scripts/LocateAndInspect.lua
@@ -1,30 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  LocateAndInspect
-
-  Summary:
-  Matching objects and inspecting image details.
-
-  Description:
-  Teaching the shape of "golden" parts and matching identical objects with full
-  rotation in the full image. Also inspecting a detail (letter count) on the part
-  to verify its correctness.
-
-  How to Run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the image viewer on the DevicePage.
-  Restarting the Sample may be necessary to show images after loading the webpage.
-  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
-  required. For example SIM4000 with latest firmware. Alternatively the Emulator
-  in AppStudio 2.3 or higher can be used..
-
-  More Information:
-  Tutorial "Algorithms - Matching".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 print('AppEngine Version: ' .. Engine.getVersion())
 
@@ -100,7 +74,7 @@ local function teach(img)
   local teachPose = matcher:teach(img, teachRegion)
 
   -- Viewing model points overlayed over teach image
-  local modelPoints = matcher:getEdgePoints() -- Model points in model's local coord syst
+  local modelPoints = matcher:getModelPoints() -- Model points in model's local coord syst
   local teachPoints = Point.transform(modelPoints, teachPose)
   for _, point in ipairs(teachPoints) do
     viewer:addShape(point, teachDecoration, nil, imageID)
